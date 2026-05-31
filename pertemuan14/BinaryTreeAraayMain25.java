d45926d1fc0594f8e29e411745fd267a8e859c2c
rayshaaidaazalia <raysa4872@gmail.com>
2026-05-31 16:34:30 +0700

    P14_Percobaan2

---
+++ pertemuan14/BinaryTreeAraayMain25.java
@@ -0,0 +1,21 @@
+package pertemuan14;
+
+public class BinaryTreeAraayMain25 {
+    public static void main(String[] args){
+        BinaryTreeArray25 bta25 = new BinaryTreeArray25();
+        mahasiswa25 mhs1 = new mahasiswa25("244160121","Ali","A",3.57);
+        mahasiswa25 mhs2 = new mahasiswa25("244160185","Candra","C",3.41);
+        mahasiswa25 mhs3 = new mahasiswa25("244160200","Badar","B",3.75);
+        mahasiswa25 mhs4 = new mahasiswa25("244160221","Dewi","B",3.35);
+
+        mahasiswa25 mhs5 = new mahasiswa25("244160185","Devi","A",3.48);
+        mahasiswa25 mhs6 = new mahasiswa25("244160205","Ehsan","D",3.61);
+        mahasiswa25 mhs7 = new mahasiswa25("244160170","Fizi","B",3.86);
+
+        mahasiswa25[] datamhs25 ={mhs1,mhs2,mhs3,mhs4,mhs5,mhs6,mhs7,null,null,null};
+        int idxLast25 = 6;
+        bta25.populateData25(datamhs25,idxLast25);
+        System.out.println("\nInorder Travelsal Mahasiswa: ");
+        bta25.traverseInOrder25(0);
+    }
+}
---
+++ pertemuan14/BinaryTreeArray25.java
@@ -0,0 +1,23 @@
+package pertemuan14;
+
+public class BinaryTreeArray25 {
+    mahasiswa25[] dataMahasiswa25;
+    int idxLast25;
+
+public BinaryTreeArray25(){
+    this.dataMahasiswa25 = new mahasiswa25[10];
+}
+void populateData25(mahasiswa25 dataMhs25[], int idxLast) {
+    this.dataMahasiswa25 = dataMhs25;
+    this.idxLast25 = idxLast;
+}
+void traverseInOrder25(int idxStart25){
+  if(idxStart25 <= idxLast25) {
+    if(dataMahasiswa25[idxStart25] != null){
+        traverseInOrder25(2*idxStart25 + 1);
+        dataMahasiswa25[idxStart25].tampilInformasi();
+        traverseInOrder25(2*idxStart25+2);
+    }
+  }
+}
+}
