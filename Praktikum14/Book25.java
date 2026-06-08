66b4a89bf505f67443ddb48a6d660f793f38d908
rayshaaidaazalia <raysa4872@gmail.com>
2026-06-08 18:49:48 +0700

    Praktikum14

---
+++ Praktikum14/Book25.java
@@ -0,0 +1,17 @@
+package Praktikum14;
+
+public class Book25 {
+    public String isbn25;
+    public String title25;
+
+public Book25() {
+
+}
+public Book25(String isbn25, String title25) {
+    this.isbn25 = isbn25;
+    this.title25 = title25;
+}
+public String toString() {
+    return "ISBN: " +this.isbn25 + "Title: " + this.title25;
+}
+}
---
+++ Praktikum14/Customer25.java
@@ -0,0 +1,19 @@
+package Praktikum14;
+
+public class Customer25 {
+    public int id25;
+    public String name25;
+
+    public Customer25() {
+
+    }
+public Customer25(int id25, String name25) {
+    this.id25 = id25;
+    this.name25 = name25;
+}
+public String toString() {
+    return "ID: " + this.id25 +" Nama: " + this.name25;
+}
+}
+
+
