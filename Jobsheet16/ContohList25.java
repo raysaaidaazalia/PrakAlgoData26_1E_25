b601053f94f0520797f1df70735d237c2dd3e13f
rayshaaidaazalia <raysa4872@gmail.com>
2026-06-16 10:10:06 +0700

    P16_Percobaan1

---
+++ Jobsheet16/ContohList25.java
@@ -0,0 +1,32 @@
+package Jobsheet16;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+
+public class ContohList25 {
+    public static void main(String[] args){
+    ArrayList l25 = new ArrayList();
+    l25.add(1);
+    l25.add(2);
+    l25.add(3);
+    l25.add("cireng");
+    System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l25.get(0), l25.size(), l25.get(l25.size() - 1));
+    
+    l25.add(4);
+    l25.remove(0);
+    System.out.printf("elemen 0: %d total elemen: %d elemen terakhir : %s\n", l25.get(0), l25.size(), l25.get(l25.size() - 1));
+
+   List<String> names25 = new LinkedList<>();
+    names25.add("Noureen");
+    names25.add("Akhleema");
+    names25.add("Shannum");
+    names25.add("Uwais");
+    names25.add("Al-Qarni");
+    System.out.printf("Elemen 0 : %s total elemen: %s elemen terakhir : %s\n", names25.get(0), names25.size(),names25.get(names25.size() - 1));
+    names25.set(0,"My kid");
+    System.out.printf("Elemen 0 : %s total elemen : %s elemen terakhir : %s\n", names25.get(0),names25.size(), names25.get(names25.size() - 1));
+    System.out.println("Names: "+names25.toString());
+    }
+   
+}
